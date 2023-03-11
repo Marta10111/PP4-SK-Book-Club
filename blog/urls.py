be79ce10df154ea44feccda62300c4482fc5e9f1
Marta <manta.101@hotmail.com>
2023-03-11 16:51:06 +0000

    Adding editing post functionality and adding new post from user

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('post_create/', views.PostCreate.as_view(), name='post_create'), 
+    path('post_create/', views.PostCreate.as_view(), name='post_create'),
+    path('post_edit/<slug:slug>', views.PostEdit.as_view(), name='post_edit'),  
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic, View
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy
 from .models import Post
@@ -31,11 +31,8 @@ class PostDetail(View):
             {
                 "post": post,
                 "comments": comments,
-                "liked": liked
-            },
-            {
                 "liked": liked,
-                "comment_form": CommentForm()
+                "comment_form": CommentForm(),
             },
         )
 
@@ -68,38 +65,7 @@ def post(self, request, slug, *args, **kwargs):
             "comment_form": CommentForm()
          },
     )
-            }
-        )
-
-    def post(self, request, slug, *args, **kwargs):
-        queryset = Post.objects
-        post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.order_by("created_on")
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
-
-        comment_form = CommentForm(data=request.POST)
-
-        if comment_form.is_valid():
-            comment_form.instance.name = request.user.username
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.save()
-        else:
-            comment_form = CommentForm()
-
-        return render(
-            request,
-            "post_detail.html",
-            {
-                "post": post,
-                "comments": comments,
-                "liked": liked,
-                "comment_form": CommentForm()
-            },
-        )
-
+            
 
 class PostLike(View):
 
@@ -116,10 +82,21 @@ class PostLike(View):
 
 class PostCreate(CreateView):
     model = Post
-    fields = ['title', 'location', 'content', 'featured_image', 'excerpt']
+    fields = ['title', 'content', 'featured_image',]
     template_name = 'post_create.html'
     success_url = reverse_lazy('home')
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super(PostCreate, self).form_valid(form)
+
+
+class PostEdit(UpdateView):
+    model = Post()
+    fields = ['title', 'content', 'featured_image']
+    template_name = 'post_edit.html'
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(PostEdit, self).form_valid(form)
